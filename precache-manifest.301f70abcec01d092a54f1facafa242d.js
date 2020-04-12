c70cff876f0fd2f8e42343fb81eca994da63d4b5
Édipo Carneiro <edipocarneiro@gmail.com>
2020-04-12 15:29:11 -0300

    Updates

---
+++ precache-manifest.301f70abcec01d092a54f1facafa242d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f11045f39a440e3e0882a2a6365e48",
+    "url": "/react-course-1/index.html"
+  },
+  {
+    "revision": "184edc41a24120772204",
+    "url": "/react-course-1/static/css/main.ab9024b8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-course-1/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course-1/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184edc41a24120772204",
+    "url": "/react-course-1/static/js/main.911e6dcb.chunk.js"
+  },
+  {
+    "revision": "ab8fbf90f641e9952284",
+    "url": "/react-course-1/static/js/runtime-main.4ffe0051.js"
+  }
+]);
